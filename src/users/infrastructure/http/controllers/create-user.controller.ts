976113b387c71c1e8064494c77195a9c766830e4
Otavin96 <otaviolazzarotto@icloud.com>
2024-12-27 23:57:53 -0300

    adicionando criptografia no password do Usuário

--- src/users/infrastructure/http/controllers/create-user.controller.ts
+++ src/users/infrastructure/http/controllers/create-user.controller.ts
@@ -1,12 +1,15 @@
 import { dataValidation } from '@/common/infrastructure/validation/zod'
 import { CreateUsersUseCase } from '@/users/application/usecases/create-user.usecase'
+import { hash } from 'bcrypt'
 import { Request, Response } from 'express'
 import { container } from 'tsyringe'
 import { z } from 'zod'
+
 export async function createUserController(
   request: Request,
   response: Response,
 ) {
+  // Validação do corpo da requisição
   const createUserSchemaBody = z.object({
     name: z.string(),
     email: z.string(),
@@ -18,10 +21,15 @@ export async function createUserController(
     request.body,
   )
 
+  // Hash da senha
+  const hashedPassword = await hash(password, 10)
+
+  // Injeção de dependência
   const createUsersUseCase: CreateUsersUseCase.UseCase =
     container.resolve('CreateUsersUseCase')
 
-  const user = await createUsersUseCase.execute({ name, email, password })
+  // Execução do caso de uso e resposta
+  const user = await createUsersUseCase.execute({ name, email, password: hashedPassword })
 
   response.status(201).json(user)
 }
